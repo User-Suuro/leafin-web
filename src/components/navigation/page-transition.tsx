35d84b13faf1eb5f236330c1c9166f40539077a0
User-Suuro <godwingalvez26@gmail.com>
2025-08-30 15:52:23 +0800

    fixed moving page transition which causes page breaks

--- src/components/navigation/page-transition.tsx
+++ src/components/navigation/page-transition.tsx
@@ -10,9 +10,9 @@ export default function PageTransition({ children }: { children: React.ReactNode
     <AnimatePresence mode="sync">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 15 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -15 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{ duration: 0.3, delay: 0.1 }}
       >
         {children}
